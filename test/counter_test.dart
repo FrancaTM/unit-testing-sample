a42e508b9a15c5762826ae76f212d26fb4192fa1
Túlio <francatm@outlook.com>
2019-05-23 02:17:49 -0300

    write test for class

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -1,1 +1,12 @@
+import 'package:test/test.dart';
+import 'package:unit_testing_sample/counter.dart';
 
+void main() {
+  test('counter value should be incremented', () {
+    final counter = Counter();
+
+    counter.increment();
+
+    expect(counter.value, 1);
+  });
+}
