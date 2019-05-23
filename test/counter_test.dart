a1ef12cdf6eae2b0f78131d9b909757edf8f23b1
Túlio <francatm@outlook.com>
2019-05-23 02:23:14 -0300

    combine multiple tests in group

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,11 +2,28 @@ import 'package:test/test.dart';
 import 'package:unit_testing_sample/counter.dart';
 
 void main() {
-  test('counter value should be incremented', () {
-    final counter = Counter();
+  group('counter', () {
+    test('value should start at zero', () {
+//      final counter = Counter();
+//      expect(counter.value, 0);
 
-    counter.increment();
+      expect(Counter().value, 0);
+    });
 
-    expect(counter.value, 1);
+    test('counter value should be incremented', () {
+      final counter = Counter();
+
+      counter.increment();
+
+      expect(counter.value, 1);
+    });
+
+    test('counter value should be decremented', () {
+      final counter = Counter();
+
+      counter.decrement();
+
+      expect(counter.value, -1);
+    });
   });
 }
