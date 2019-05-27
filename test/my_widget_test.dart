12a55e9f03fe60ed7201f8a5acec607a5cea7e8d
Túlio <francatm@outlook.com>
2019-05-27 16:42:40 -0300

    create widget to test create testWidgets test build widget using WidgetTester search widget using Finder verify widget working using Matcher

---
+++ test/my_widget_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:unit_testing_sample/my_widget.dart';
+
+void main() {
+  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
+    await tester.pumpWidget(MyWidget(title: 'T', message: 'M'));
+
+    final titleFinder = find.text('T');
+    final messageFinder = find.text('M');
+
+    expect(titleFinder, findsOneWidget);
+    expect(messageFinder, findsOneWidget);
+  });
+}
