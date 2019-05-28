9c375de7050d675904814a8490435deae3d81db0
Túlio <francatm@outlook.com>
2019-05-28 01:02:06 -0300

    find text widget

--- test/my_widget_test.dart
+++ test/my_widget_test.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:unit_testing_sample/my_widget.dart';
@@ -11,5 +13,15 @@ void main() {
 
     expect(titleFinder, findsOneWidget);
     expect(messageFinder, findsOneWidget);
+  });
+
+  testWidgets('finds a Text Widget', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: Text('H'),
+      ),
+    ));
+
+    expect(find.text('H'), findsOneWidget);
   });
 }
